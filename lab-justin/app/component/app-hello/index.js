6a1f1b931e1d4c1f8f008551d5be896df7cb88d6
Sung J Byun <sungjbyun@D-173-250-161-77.dhcp4.washington.edu>
2016-07-29 16:28:37 -0700

    push b4 change

--- lab-justin/app/component/app-hello/index.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-require('./app-hello-directive.js');
-require('./app-hello-controller.js');
-require('./app-hello.scss');
--- lab-justin/app/entry.js
+++ lab-justin/app/entry.js
@@ -1,15 +1,12 @@
 'use strict';
 
-
 require('!!file?name=[name].[ext]!./html/index.html');
 require('./sass/base.scss');
 
 const angular = require('angular');
-
 angular.module('galleryApp', []);
 
 require('./component/app-main');
-require('./component/app-hello');
 require('./component/app-gallery');
 require('./component/app-gallery-item');
 require('./component/app-edit-gallery-item-form');
